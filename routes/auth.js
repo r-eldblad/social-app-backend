6c2da41bc678dbc9b02857cf640db443f8081462
Rasmus Eldblad <r.eldblad@protonmail.com>
2021-09-21 23:58:31 +0200

    This commit adds jwt authentication and adds functionality for hashing passwords when user registers"

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send('Email already exist');
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
   try {
     const savedUser = await user.save();
@@ -13,6 +21,20 @@ router.post('/register', async (req, res) => {
   } catch (err) {
     res.status(400).send(err);
   }
+});
+
+router.post('/login', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Email does not exist.');
+  // Check if password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+
+  if (!validPass) return res.status(400).send('Invalid password.');
+
+  // CReate and assign a token
+
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header('auth-token', token).send(token);
 });
 
 module.exports = router;
